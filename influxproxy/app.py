d0cab40be58a138c2b6b965e1a1fea72cafb4c61
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2016-08-23 03:15:22 -0300

    Basic preflight endpoint

--- influxproxy/app.py
+++ influxproxy/app.py
@@ -6,16 +6,57 @@ from influxproxy.configuration import DEBUG, PORT, config, logger
 from influxproxy.drivers import InfluxDriver
 
 
+ALL_ALLOWED_ORIGINS = [
+    allowed
+    for db_conf in config['databases'].values()
+    for allowed in db_conf['allow_from']
+]
+
+
+class RequestError(RuntimeError):
+    """Raised when there's a request error."""
+
+
 def create_app(loop):
     app = web.Application(logger=logger, loop=loop, debug=DEBUG)
 
     app.router.add_route('GET', '/ping', ping)
+    app.router.add_route('OPTIONS', '/metric', preflight_metric)
 
     return app
 
 
+def ensure_headers(request):
+    expected_headers = ['Origin', 'Access-Control-Request-Method']
+    for header in expected_headers:
+        if header not in request.headers:
+            raise RequestError('{} header is missing'.format(header))
+
+
 async def ping(request):
     return web.Response(body=b'pong')
+
+
+async def preflight_metric(request):
+    try:
+        ensure_headers(request)
+    except RequestError as e:
+        return web.Response(status=400, reason=str(e).encode('utf-8'))
+
+    origin = request.headers['Origin']
+
+    if request.headers['Access-Control-Request-Method'] != 'POST':
+        return web.Response(status=405, reason=b'Only POST allowed')
+
+    if origin not in ALL_ALLOWED_ORIGINS:
+        return web.Response(status=403, reason=b'Origin not allowed')
+
+    return web.Response(body=b'', headers={
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Request-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': origin,
+    })
 
 
 if __name__ == '__main__':  # pragma: no cover
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,10 @@ from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from nose.tools import istest
 
 from influxproxy.app import create_app
+from influxproxy.configuration import config
+
+
+DB_CONF = config['databases']['testing']
 
 
 class PingTest(AioHTTPTestCase):
@@ -17,3 +21,59 @@ class PingTest(AioHTTPTestCase):
         content = await response.text()
         expected = 'pong'
         self.assertEqual(content, expected)
+
+    @istest
+    @unittest_run_loop
+    async def sends_a_metric_preflight(self):
+        response = await self.client.options('/metric', headers={
+            'Origin': DB_CONF['allow_from'][0],
+            'Access-Control-Request-Method': 'POST',
+        })
+
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.headers['Access-Control-Allow-Origin'],
+                         DB_CONF['allow_from'][0])
+        self.assertEqual(response.headers['Access-Control-Allow-Credentials'],
+                         'true')
+        self.assertEqual(response.headers['Access-Control-Allow-Methods'],
+                         'POST')
+        self.assertEqual(response.headers['Access-Control-Request-Headers'],
+                         'Content-Type')
+
+    @istest
+    @unittest_run_loop
+    async def cannot_accept_preflight_if_origin_not_expected(self):
+        response = await self.client.options('/metric', headers={
+            'Origin': 'some-bogus_origin',
+            'Access-Control-Request-Method': 'POST',
+        })
+
+        self.assertEqual(response.status, 403)
+
+    @istest
+    @unittest_run_loop
+    async def cannot_accept_preflight_if_method_not_expected(self):
+        response = await self.client.options('/metric', headers={
+            'Origin': DB_CONF['allow_from'][0],
+            'Access-Control-Request-Method': 'GET',
+        })
+
+        self.assertEqual(response.status, 405)
+
+    @istest
+    @unittest_run_loop
+    async def cannot_accept_preflight_if_missing_origin(self):
+        response = await self.client.options('/metric', headers={
+            'Access-Control-Request-Method': 'POST',
+        })
+
+        self.assertEqual(response.status, 400)
+
+    @istest
+    @unittest_run_loop
+    async def cannot_accept_preflight_if_missing_method(self):
+        response = await self.client.options('/metric', headers={
+            'Origin': DB_CONF['allow_from'][0],
+        })
+
+        self.assertEqual(response.status, 400)
